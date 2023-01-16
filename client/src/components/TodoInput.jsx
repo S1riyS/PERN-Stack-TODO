ebdedcb1131090f3a3154d369c054628144e1953
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-01-16 17:13:37 +0500

    feat: functions moved to `App.js`

--- client/src/components/TodoInput.jsx
+++ client/src/components/TodoInput.jsx
@@ -1,24 +1,14 @@
 import React, {useState} from 'react';
 
-const TodoInput = ({todos, setTodos}) => {
+const TodoInput = ({create}) => {
     const [description, setDescription] = useState("");
 
     const onSubmitForm = async e => {
         e.preventDefault();
-        try {
-            const body = {description};
-            const response = await fetch("http://localhost:5000/api/todo", {
-                method: "POST",
-                headers: {"Content-Type": "application/json"},
-                body: JSON.stringify(body)
-            });
-            const newTodo = await response.json();
-            setTodos([...todos, newTodo])
-            setDescription('')
-        } catch (err) {
-            console.error(err.message);
-        }
+        create(description)
+        setDescription('')
     };
+
     return <>
         <h1 className="text-center mt-5">TODO List</h1>
         <form className="d-flex mt-5 mb-4" onSubmit={onSubmitForm}>
--- client/src/components/TodoItem.js
+++ client/src/components/TodoItem.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
-const TodoItem = ({todo, deleteTodo}) => {
-
+const TodoItem = ({todo, remove}) => {
     return (
         <div className='todo__item'>
             {todo.description}
             <button
                 className="btn btn-danger"
-                onClick={() => deleteTodo(todo.id)}
+                onClick={() => remove(todo.id)}
             >
                 Delete
             </button>
